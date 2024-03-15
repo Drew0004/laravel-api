b6e5b54f2f7a39ba22f7796a5a2e39979a4a36e7
Drew0004 <drdrewgraphic@gmail.com>
2024-03-15 17:44:18 +0100

    Add Contact data to Contacts Table in database

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -6,11 +6,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Http\Requests\StoreContactRequest;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
     public function store(StoreContactRequest $request)
     {
-        return response()-> json($request->validated());
+        $contact = Contact::create($request->validated());
+        return response()-> json([
+            'success' => true,
+            'message' => 'Contatto salvato con successo',
+        ]);
     }
 }
