0b8a8b61c5c896f9882b531f3a61c788a4d41736
Drew0004 <drdrewgraphic@gmail.com>
2024-03-15 17:32:02 +0100

    Add data backend validation

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\StoreContactRequest;
+
 class ContactController extends Controller
 {
-    public function store(Request $request)
+    public function store(StoreContactRequest $request)
     {
-        return response()-> json($request->all());
+        return response()-> json($request->validated());
     }
 }
---
+++ app/Http/Requests/StoreContactRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'firstname' => 'required| max:74',
+            'lastname' => 'required| max:74',
+            'email' => 'required| max: 255',
+            'message' => 'required| max:1024',
+            'accepted' => 'required|boolean|accepted',
+
+        ];
+    }
+}
